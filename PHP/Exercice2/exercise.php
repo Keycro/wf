2e7d5b15609588f23572eb6a842417a9aa935389
Karlo Cazar <karlo.cazar@gmail.com>
2019-02-25 12:19:21 +0100

    Exercise 2 done

--- PHP/Exercice2/exercise.php
+++ PHP/Exercice2/exercise.php
@@ -2,4 +2,30 @@
 $password;
 $salt;
 
-$saltedPassword = '';
+
+// step 1 - take the first half of the password
+
+$firstpart =substr
+($password,
+    0,
+    floor(strlen($password)/2)+(strlen($password)%2));
+
+// step 2 concat with the var
+
+$lastpart =substr($password,ceil(strlen($password)/2));
+
+$saltedPassword = $firstpart . $salt . $lastpart;
+
+
+/*$pos = ceil(strlen($password)/2);
+
+// step 1 - take the first half of the password
+
+$firstpart =substr($password,0,$pos);
+
+// step 2 concat with the var
+
+$secondpart =substr($password,$pos);
+
+$saltedPassword = $firstpart.$salt.$secondpart;*/
+
