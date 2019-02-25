35e2c1cedf85219a2b4d09fd1dc1866f7fa52211
Karlo Cazar <karlo.cazar@gmail.com>
2019-02-25 15:13:17 +0100

    Correction of exercise 3

--- PHP/Exercice3/exercise.php
+++ PHP/Exercice3/exercise.php
@@ -1,4 +1,21 @@
 <?php
 
-$input;
-$winner;
+$input = [[1,2], [3,2], [5,6],[7,2]];
+$brotherA = 0;
+$brotherB = 0;
+
+foreach ($input as $play){
+    if( $play[0]>$play[1]){
+
+         $brotherA++;
+    }else if ($play[0] < $play[1]){
+        $brotherB++;
+    }
+}
+
+
+if ($brotherA > $brotherB) {
+    $winner = 'A';
+} else {
+    $winner = 'B';
+}
