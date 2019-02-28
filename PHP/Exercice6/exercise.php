f635b867c5151d645eb975f37e5d8a3899cc0bd1
Karlo Cazar <karlo.cazar@gmail.com>
2019-02-28 14:58:41 +0100

    Exercise Jose 6 done

--- PHP/Exercice6/exercise.php
+++ PHP/Exercice6/exercise.php
@@ -1,32 +1,28 @@
 <?php
 
-/*To create an easter egg, your dev leader create a function that shuffle a file. You find this easter egg really dangerous because no rollback can be done.
 
-You decide to create a function called "easterReverse" that take a file path as argument, to reverse the second half part of the file.
+function easterReverse(string $filetowrite){
 
-Tips : Use "floor(strlen($fileContent) / 2)" to calculate the middle of the file.
-Warning : you are not allowed to use "file_put_contents", "w" or "w+" access mode.
+    $filecontent = file_get_contents($filetowrite);
 
-To validate it, just cd into the Exercice6 folder and run "php phpunit-6.5.5.phar".*/
+    //echo "$filecontent";
 
+    //echo "<br>Halfway: ".floor(strlen($filecontent)/2);
 
-$file = "file.txt";
-$filetowrite = "fileTemp.txt";
+    $secondPart = substr($filecontent, floor(strlen($filecontent)/2));
+    $firstpart = substr($filecontent,0,strlen($secondPart)-1);
 
-copy($file,$filetowrite);
+    //echo "<br>firstpart:<br>".$firstpart;
 
+    //echo "<br>secondPart:<br>".$secondPart;
 
-$filecontent = file_get_contents($filetowrite);
+    //echo "<br>secondPartReversed:<br>".strrev($secondPart);
 
-echo "$filecontent";
+    $file = fopen($filetowrite, "r+");
+    fseek($file,strlen($firstpart),SEEK_SET);
+    fwrite($file,strrev($secondPart),strlen($secondPart));
+    fclose($file);
 
-echo "<br>Halfway: ".floor(strlen($filecontent)/2);
+}
 
-$secondPart = substr($filecontent,floor(strlen($filecontent)/2));
-$firstpart = substr($filecontent,0,strlen($secondPart)-1);
-
-echo "<br>firstpart:<br>".$firstpart;
-
-echo "<br>secondPart:<br>".$secondPart;
-
-echo "<br>secondPartReversed:<br>".strrev($secondPart);
+easterReverse("file.txt");
