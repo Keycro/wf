d81c9e36717a56d31b07f0adafffa8d155fae087
Karlo Cazar <karlo.cazar@gmail.com>
2019-03-01 14:18:02 +0100

    Exercise 7 Jose done

--- PHP/Exercice7/exercise.php
+++ PHP/Exercice7/exercise.php
@@ -1,1 +1,34 @@
 <?php
+
+function divide(int $Value, int $by ): float {
+    if ($by == 0) {
+        throw new RuntimeException("Division by 0 is not allowed");
+    }
+    return (float)($Value/$by);
+}
+/*
+try {
+    divide(10,0);
+}   catch (RuntimeException $exception) {
+    echo $exception->getMessage();
+}*/
+
+function arrayDivide(array $baseArray, int $by) : array {
+    $result = [];
+
+    foreach ($baseArray as $base) {
+    try {
+        echo "VALUE: $base ";
+        $result[] = divide($base,$by);
+    } catch (RuntimeException $exception) {
+        echo "<br>".$exception->getMessage();
+        echo "<br> Using ".$base;
+        $result[] = $base;
+    }
+
+    }
+
+    return $result;
+}
+
+print_r(arrayDivide([15,5,6],0));
