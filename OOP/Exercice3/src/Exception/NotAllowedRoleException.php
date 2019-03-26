b313136a5144d91b5287fdb5c86a65ae5d54ec57
Karlo Cazar <karlo.cazar@gmail.com>
2019-03-26 15:43:12 +0100

    Exercise 3 done

---
+++ OOP/Exercice3/src/Exception/NotAllowedRoleException.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace Exception;
+
+
+use Throwable;
+
+class NotAllowedRoleException extends \RuntimeException
+{
+    public function __construct(array $roles, string $label, string $message = "", int $code = 0, Throwable $previous = null)
+    {
+        $tmpMessage = 'The role'.$label.'is not allowed';
+        $tmpMessage .= 'only'.implode(',',$roles).'are allowed';
+        $tmpMessage .= $message;
+
+        parent::__construct($tmpMessage, $code, $previous);
+    }
+
+}
